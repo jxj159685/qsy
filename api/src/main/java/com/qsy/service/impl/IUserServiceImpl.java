9d57b05862df49a213286cc30fcc81b41e411ffb
jxj159685 <310907873@qq.com>
2018-05-13 16:49:11 +0800

    好吃

--- api/src/main/java/com/qsy/service/impl/IUserServiceImpl.java
+++ api/src/main/java/com/qsy/service/impl/IUserServiceImpl.java
@@ -24,6 +24,7 @@ public class IUserServiceImpl implements IUserService {
 
     @Override
     public ServerResponse getProductList() {
+        //不好吃
         return ServerResponse.createBySuccess(productMapper.selectByPrimaryKey("1"));
     }
 }
